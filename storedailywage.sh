29ce088d4be8065399565a9ab6374e9282377f3c
karthik <nallaboluk@gmail.com>
2020-05-21 11:05:48 +0530

    adding storedailywage

---
+++ storedailywage.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+IsPartTime=1;
+IsFullTime=2;
+MaxHrsInMonth=100;
+EmpRatePerHour=20;
+NumWorkingDays=20;
+
+totalEmpHrs=0;
+totalWorkingDays=0;
+
+function getWorkingHours()
+{
+empCheck=$((RANDOM%3))
+   case $empCheck in
+        $IsFullTime)
+         empHrs=8
+          ;;
+         $IsPartTime)
+          empHrs=4
+           ;;
+           *)
+           empHrs=0
+           ;;
+    esac
+return $empHrs
+}
+counter=0
+while [[ $totalEmpHrs -lt $MaxHrsInMonth && $totalWorkingDays -lt $NumWorkingDays ]] 
+do
+    totalWorkingDays=$(($totalWorkingDays+1))
+    getWorkingHours
+    empHrs=$?
+    dailyWage[((counter++))]=$(($empHrs*$EmpRatePerHour))
+    totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+totalSalary=$(($totalEmpHrs*$EmpRatePerHour))
+
